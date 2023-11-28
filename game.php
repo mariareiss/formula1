9d036ade07b95b690fb9b0d7c3f4bca2c1b56898
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-27 18:39:59 -0600

    Update game.php

--- game.php
+++ game.php
@@ -19,18 +19,18 @@ $racers = [
 // Simulate the race
 echo "Formula 1 Race Simulation\n\n";
 
-$trackLength = 5; // Length of the track
-$raceDistance = 30; // Total distance of the race
+$trackLength = 20; // Length of the track
+$raceDistance = 100; // Total distance of the race
 
 $raceResults = []; // Array to store race results
 
 foreach ($racers as $racer) {
     $position = 0; // Starting position
     for ($distance = 0; $distance < $raceDistance; $distance += $trackLength) {
-        $speed = rand(80, 120); // Randomize speed
+        $speed = rand(5, 15); // Randomize speed
         $position += $speed; // Update position
-        echo "$racer at $distance km\n";
-        usleep(500000); // Delay for visualization purposes (0.5 seconds)
+        usleep(50000); // Delay for visualization purposes (0.05 seconds)
+        displayRaceTrack($racers, $racer, $position, $raceDistance);
     }
     $raceResults[$racer] = $position; // Store racer and final position
 }
@@ -42,4 +42,23 @@ $position = 1;
 foreach ($raceResults as $racer => $finalPosition) {
     echo "$position. $racer\n";
     $position++;
+}
+
+// Function to display the race track and racers' positions
+function displayRaceTrack($racers, $currentRacer, $position, $raceDistance)
+{
+    $trackWidth = 30; // Width of the track display
+
+    echo "Race Track:\n";
+    foreach ($racers as $r) {
+        $racerPosition = round(($position / $raceDistance) * $trackWidth);
+        if ($r === $currentRacer) {
+            echo str_pad($r, $racerPosition, '_') . "X\n";
+        } else {
+            echo str_pad($r, $trackWidth, '_') . "\n";
+        }
+    }
+    echo str_repeat('-', $trackWidth) . "\n";
+    usleep(50000); // Delay for visualization purposes (0.05 seconds)
+    echo "\033[2J\033[;H"; // Clear console for updated display
 }
