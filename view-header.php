7a343fd5de107aa0aa203015e01fb5348771dcf6
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-27 22:48:56 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,12 +7,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
      body {
-  background-image: url('f1-image.avif'); /* Replace 'path/to/your/image.jpg' with your image path */
-  background-size: cover; /* Adjust the background size as needed */
-  background-repeat: no-repeat; /* Prevent the image from repeating */
-  opacity: 1.0; /* Adjust the opacity to make the background slightly transparent */
+  background-image: url('f1-image.avif'); 
+  background-size: cover; 
+  background-repeat: no-repeat; 
+  opacity: 1.0; 
 }
-
     </style>
 </head>
 <body>
