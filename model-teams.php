8078c102ea3bdc52d387e4e8703dc1834a8390e3
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-29 20:25:12 -0600

    Update model-teams.php

--- model-teams.php
+++ model-teams.php
@@ -1,9 +1,9 @@
 <?php
-function selectTeams($Iid) {
+function selectTeams($iid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT Drivers.driver_name, Teams.team_name, Teams.team_id, Teams.team_principal FROM Drivers JOIN Teams ON Drivers.team_id = Teams.team_id WHERE Drivers.driver_id = ?;");
-        $stmt->bind_param("i", $Iid);  
+        $stmt->bind_param("i", $iid);  
       $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
