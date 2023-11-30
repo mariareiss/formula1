f88047ab3a1ba37d9952d6b6d97f94e630456d05
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-29 19:18:51 -0600

    Update model-races.php

--- model-races.php
+++ model-races.php
@@ -2,7 +2,7 @@
 function selectRaces() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT race_name, race_location, race_laps, race_winner, race_team_winner FROM `race`");
+        $stmt = $conn->prepare("SELECT race_name, race_location, race_laps, race_winner, race_team_winner FROM `races`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
