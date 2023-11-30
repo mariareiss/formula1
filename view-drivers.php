51cbc4ae8df19a9bbfebdeed0ad67727ed444751
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-29 18:45:09 -0600

    Create view-drivers.php

---
+++ view-drivers.php
@@ -0,0 +1,27 @@
+<h1>Drivers</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+         <th>Driver Name</th>
+      <th>Nationality</th>
+      <th>Age</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+  <?php
+  while ($driver = $drivers->fetch_assoc()) {
+    ?>
+      <tr>
+        <td><?php echo $driver['driver_name']; ?></td>
+        <td><?php echo $driver['driver_nationality']; ?></td>
+        <td><?php echo $driver['driver_age']; ?></td>
+        <td><a href="team-of-driver.php?id=<?php echo $driver['driver_id']; ?>">Driver Team</a></td>
+      </tr>
+    <?php
+  }
+  ?>
+    </tbody>
+  </table>
+</div>
