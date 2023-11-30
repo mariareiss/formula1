8e4e99675760f6a1e3e44917a2462b0aa459fac4
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-29 19:17:57 -0600

    Create model-races.php

---
+++ model-races.php
@@ -0,0 +1,15 @@
+<?php
+function selectRaces() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT race_name, race_location, race_laps, race_winner, race_team_winner FROM `race`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
