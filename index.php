d2c48277117f0262ad4270f1f6a241491b341af5
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-29 20:43:05 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 $pageTitle = "Home";
-include "view-header.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,15 +8,12 @@ include "view-header.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
         body {
-            display: flex;
-            min-height: 100vh;
-            margin: 0;
-            justify-content: center;
-            align-items: center;
+            text-align: center;
         }
 
-        div {
-            text-align: center;
+        h1 {
+            margin: 0 auto; /* Centering the h1 horizontally */
+            padding: 50px; /* Optional: Add padding for better visual appearance */
         }
     </style>
 </head>
@@ -26,7 +22,9 @@ include "view-header.php";
         <h1>Welcome to your Formula 1 Portal</h1>
     </div>
     <?php
+    include "view-header.php";
     include "view-footer.php";
     ?>
 </body>
 </html>
+
