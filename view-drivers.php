a1b796bb04cc56bfc91d534f3eadfba7ecdfc78f
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-29 20:43:10 -0600

    Update view-drivers.php

--- view-drivers.php
+++ view-drivers.php
@@ -1,27 +1,59 @@
-<h1>Drivers</h1>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-         <th>Driver Name</th>
-      <th>Nationality</th>
-      <th>Age</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-  <?php
-  while ($driver = $drivers->fetch_assoc()) {
-    ?>
-      <tr>
-        <td><?php echo $driver['driver_name']; ?></td>
-        <td><?php echo $driver['driver_nationality']; ?></td>
-        <td><?php echo $driver['driver_age']; ?></td>
-        <td><a href="teams.php?id=<?php echo $driver['driver_id']; ?>">Driver Team</a></td>
-      </tr>
-    <?php
-  }
-  ?>
-    </tbody>
-  </table>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Drivers</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: #f8f9fa; /* Set your desired background color */
+        }
+        .container {
+            margin-top: 20px;
+        }
+        th {
+            font-weight: bold;
+        }
+        td, th {
+            text-align: center;
+            vertical-align: middle !important;
+        }
+        .driver-link {
+            color: #007bff; /* Set the link color */
+            text-decoration: none;
+        }
+        .driver-link:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Drivers</h1>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Driver Name</th>
+                        <th>Nationality</th>
+                        <th>Age</th>
+                        <th>Driver Team</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($driver = $drivers->fetch_assoc()) { ?>
+                        <tr>
+                            <td><?php echo $driver['driver_name']; ?></td>
+                            <td><?php echo $driver['driver_nationality']; ?></td>
+                            <td><?php echo $driver['driver_age']; ?></td>
+                            <td><a href="teams.php?id=<?php echo $driver['driver_id']; ?>" class="driver-link">Driver Team</a></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
+
