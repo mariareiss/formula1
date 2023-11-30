eb0b48980d057b368922e8eb666012007d19190f
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-29 20:10:07 -0600

    Update teams.php

--- teams.php
+++ teams.php
@@ -5,8 +5,7 @@ require_once("model-teams.php");
 $pageTitle = "Teams";
 include "view-header.php";
 
-$did = isset($_POST['did']) ? $_POST['did'] : null;
-$teams = $did ? selectTeams($did) : null;
+$teams = selectTeams($_POST['did']) 
 
 include "view-teams.php";
 include "view-footer.php";
