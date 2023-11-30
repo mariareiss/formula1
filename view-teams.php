b4fd7cb8ae93d0710f5ea3193510a721aa8aa365
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-29 19:00:11 -0600

    Update view-teams.php

--- view-teams.php
+++ view-teams.php
@@ -1,1 +1,27 @@
-
+<h1>Teams</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Principal</th> 
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($team = $teams-> fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $team['team_id']; ?></td>
+    <td><?php echo $team['team_name']; ?></td>
+    <td><?php echo $team['team_principal']; ?></td>
+    <td><a href="products-purchased-customer.php?id=<?php echo $customer['customer_id']; ?>">Classes</a></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
