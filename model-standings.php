4f178b3532d4cd922ca09bd9e98c8aa3785aa34d
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-29 20:59:06 -0600

    Update model-standings.php

--- model-standings.php
+++ model-standings.php
@@ -2,7 +2,8 @@
 function selectStandings() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT season, points, wins, podiums FROM `Standing`");
+        $stmt = $conn->prepare("SELECT d.driver_name, season, points, wins, podiums
+FROM Drivers d join Results rs on d.driver_id=rs.driver_id join Standing s on rs.driver_id=s.driver_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
