98bc7b3d6de1558fc7698672e04f3935cb993190
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-29 19:25:40 -0600

    Update model-teams.php

--- model-teams.php
+++ model-teams.php
@@ -1,11 +1,11 @@
-<?php
-function selectTeams() {
+function selectTeams($driver_id) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT Drivers.driver_name, Teams.team_name
-FROM Drivers
-JOIN Teams ON Drivers.team_id = Teams.team_id
-WHERE Drivers.driver_id = ?; ");
+            FROM Drivers
+            JOIN Teams ON Drivers.team_id = Teams.team_id
+            WHERE Drivers.driver_id = ?");
+        $stmt->bind_param("i", $driver_id); // Assuming driver_id is an integer
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -15,4 +15,4 @@ WHERE Drivers.driver_id = ?; ");
         throw $e;
     }
 }
-?>
+
