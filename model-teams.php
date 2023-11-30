0e07f512363b8128fc9c39ef6acbfedd99edec19
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-29 19:35:24 -0600

    Update model-teams.php

--- model-teams.php
+++ model-teams.php
@@ -3,7 +3,7 @@ function selectTeams() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare
-("SELECT Drivers.driver_name, Teams.team_name
+("SELECT Drivers.driver_name, Teams.team_name, Teams.team_id, Teams.team_principal
 FROM Drivers
 JOIN Teams ON Drivers.team_id = Teams.team_id
 WHERE Drivers.driver_id = 1; -- Replace 1 with the actual driver_id you are looking for");
