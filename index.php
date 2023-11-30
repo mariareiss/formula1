323c717e00cfb3305ff138e6428789c3e2aaa22d
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-11-29 20:40:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
 <?php
 $pageTitle = "Home";
 ?>
-    <h1>Welcome to your Formula 1 Portal</h1>
-<?php
-include "view-header.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            display: flex;
+            min-height: 100vh;
+            margin: 0;
+            justify-content: center;
+            align-items: center;
+        }
 
-include "view-footer.php";
-?>
+        div {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <h1>Welcome to your Formula 1 Portal</h1>
+    </div>
+    <?php
+    include "view-header.php";
+    include "view-footer.php";
+    ?>
+</body>
+</html>
