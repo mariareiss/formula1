c1f840faa3e0473c2bb9482d08fbe27c70c75eeb
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-29 20:00:03 -0600

    Update view-teams.php

--- view-teams.php
+++ view-teams.php
@@ -3,24 +3,25 @@
   <table class="table">
     <thead>
       <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Principal</th> 
+        <th>ID</th>
+        <th>Name</th>
+        <th>Principal</th>
         <th></th>
       </tr>
     </thead>
     <tbody>
-<?php
-while ($teams = $team-> fetch_assoc()) {
-?>
-  <tr>
-    <td><?php echo $teams['team_id']; ?></td>
-    <td><?php echo $teams['team_name']; ?></td>
-    <td><?php echo $teams['team_principal']; ?></td>
-  </tr>
-<?php
-}
-?>
+      <?php
+      while ($team = $teams->fetch_assoc()) {
+      ?>
+        <tr>
+          <td><?php echo $team['team_id']; ?></td>
+          <td><?php echo $team['team_name']; ?></td>
+          <td><?php echo $team['team_principal']; ?></td>
+        </tr>
+      <?php
+      }
+      ?>
     </tbody>
   </table>
 </div>
+
