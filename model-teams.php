78065bc85df6e43279000118e07c08c9289356ed
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-11-29 19:36:23 -0600

    Update model-teams.php

--- model-teams.php
+++ model-teams.php
@@ -6,7 +6,7 @@ function selectTeams() {
 ("SELECT Drivers.driver_name, Teams.team_name, Teams.team_id, Teams.team_principal
 FROM Drivers
 JOIN Teams ON Drivers.team_id = Teams.team_id
-WHERE Drivers.driver_id = 1; -- Replace 1 with the actual driver_id you are looking for");
+WHERE Drivers.driver_id = ?; -- Replace 1 with the actual driver_id you are looking for");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
