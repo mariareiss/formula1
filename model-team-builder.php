889c0e37f8a4bdc4320418370a02c2e3b9aeac3c
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-12-04 14:52:55 -0600

    Update model-team-builder.php

--- model-team-builder.php
+++ model-team-builder.php
@@ -1,9 +1,9 @@
 <?php
-function selectBuilder($tbid) {
+function selectBuilder($bbid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT driver_b_name, team_b_name, team_principal_name, team_b_season FROM `Team_Builder` ");
-        $stmt->bind_param("i", $tbid);  
+        $stmt->bind_param("i", $bbid);  
       $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
