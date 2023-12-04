5764a03068eb9aeed30d9fcf50179601580c025d
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-04 16:04:15 -0600

    Update team-builder.php

--- team-builder.php
+++ team-builder.php
@@ -4,7 +4,7 @@ require_once("model-team-builder.php");
 
 $pageTitle = "Build your Team";
 include "view-header.php";
-$teambuilder = selectTeamBuilder();
+$teambuilder = selectTeamBuilder($_GET['id']);
 include "view-team-builder.php";
 include "view-footer.php";
 ?>
