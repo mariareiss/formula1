8448f64e2ef097dbe3ed4eece3044bbbd2d57a03
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-12-04 17:31:08 -0600

    Update drivers.php

--- drivers.php
+++ drivers.php
@@ -8,21 +8,21 @@ if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']){
     case "Add":
     if (insertDrivers($_POST['dName'], $_POST['dNationality'], $_POST['dAge'])){
-      echo '<div class="alert alert-success" role="alert">Customer added.</div>';
+      echo '<div class="alert alert-success" role="alert">Driver added.</div>';
     } else {
       echo '<div class="alert alert-error" role="alert">Error</div>';      
     }
     break;
     case "Edit":
     if (updateDrivers($_POST['dName'], $_POST['dNationality'], $_POST['dAge'], $_POST['drid'])){
-      echo '<div class="alert alert-success" role="alert">Customer edited.</div>';
+      echo '<div class="alert alert-success" role="alert">Driver edited.</div>';
     } else {
       echo '<div class="alert alert-error" role="alert">Error</div>';      
     }
     break;
     case "Delete":
     if (deleteDrivers($_POST['drid'])){
-      echo '<div class="alert alert-success" role="alert">Customer deleted.</div>';
+      echo '<div class="alert alert-success" role="alert">Driver deleted.</div>';
     } else {
       echo '<div class="alert alert-error" role="alert">Error</div>';      
     }
