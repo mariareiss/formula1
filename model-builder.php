39ca0fcd73a47f926a624bc818df900ce7711834
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-04 16:31:01 -0600

    Update model-builder.php

--- model-builder.php
+++ model-builder.php
@@ -1,11 +1,11 @@
 <?php
-function selectBuilder() {
+function selectBuilder($bid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT driver_b_name, team_b_name, team_principal_name, team_b_season FROM `Builder`");
         $stmt->bind_param();  
       $stmt->execute();
-        $result = $stmt->get_result();
+        $result = $stmt->get_result("i",$bid);
         $conn->close();
         return $result;
     } catch (Exception $e) {
