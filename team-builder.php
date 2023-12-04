66b8f6466f30da751344f8047ba77ccd74e21115
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-12-04 11:17:19 -0600

    Create team-builder.php

---
+++ team-builder.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-team-builder.php");
+
+$pageTitle = "Build your Team";
+include "view-header.php";
+$teambuilder = selectTeamBuilder();
+include "view-team-builder.php";
+include "view-footer.php";
+?>
