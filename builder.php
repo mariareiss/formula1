e7746a24e51fe41d6b1cccc01c1d5ed013e135cd
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-04 16:33:42 -0600

    Update builder.php

--- builder.php
+++ builder.php
@@ -4,7 +4,7 @@ require_once("model-builder.php");
 
 $pageTitle = "Team Builder";
 include "view-header.php";
-$builders = selectBuilder($_GET['bid']);
+$builders = selectBuilder($_GET['team_builder_id']);
 include "view-builder.php";
 include "view-footer.php";
 ?>
