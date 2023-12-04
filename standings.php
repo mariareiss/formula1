b61a97f3af0065cd6d7eeb668ee49df0d1a37ae3
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-04 16:55:36 -0600

    Update standings.php

--- standings.php
+++ standings.php
@@ -4,6 +4,35 @@ require_once("model-standings.php");
 
 $pageTitle = "Standings";
 include "view-header.php";
+
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertStanding($_POST['wins'])) {
+        echo '<div class="alert alert-success" role="alert"> Total Wins Updated! </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error! </div>';
+      }
+      break;
+    case "Edit":
+      if (updateStanding($_POST['wins'], $_POST['standing_id'])) {
+        echo '<div class="alert alert-success" role="alert"> Wins Edited! </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error! </div>';
+      }
+      break;
+        case "Delete":
+      if (deleteStanding($_POST['standing_id'])) {
+        echo '<div class="alert alert-success" role="alert"> Wins Deleted! </div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert"> Error! </div>';
+      }
+      break;
+  }
+}
+
+
 $standings = selectStandings();
 include "view-standings.php";
 include "view-footer.php";
