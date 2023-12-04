36c10d3347fbb0b83e836149185be5d1cbda3e94
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-04 10:54:30 -0600

    Update view-drivers.php

--- view-drivers.php
+++ view-drivers.php
@@ -57,3 +57,14 @@
 </body>
 </html>
 
+
+<nav aria-label="Page navigation">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
+
