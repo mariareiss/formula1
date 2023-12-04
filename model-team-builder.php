1be54fc37949df464e9ddc203bb6c7a026798274
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-12-04 11:27:44 -0600

    Create model-team-builder.php

---
+++ model-team-builder.php
@@ -0,0 +1,16 @@
+<?php
+function selectTeamBuilder($tbid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT driver_b_name, team_b_name, team_principal_name, team_b_season FROM `Team_Builder` ");
+        $stmt->bind_param("i", $tbid);  
+      $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
