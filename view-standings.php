073578ecb830e59b3f963e6d3a753e2430bdaaa4
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-12-04 10:51:42 -0600

    Update view-standings.php

--- view-standings.php
+++ view-standings.php
@@ -6,51 +6,27 @@
     <title>Standing</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
-    body {
-      font-family: Arial, sans-serif;
-      background-color: #f4f4f4;
-      margin: 0;
-      padding: 20px;
-    }
-
-    h1 {
-      text-align: center;
-      margin-bottom: 20px;
-      color: #333;
-    }
-
-    .table-responsive {
-      overflow-x: auto;
-    }
-
-    table {
-      width: 100%;
-      border-collapse: collapse;
-      background-color: #fff;
-      border-radius: 8px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
-
-    th, td {
-      padding: 12px 15px;
-      text-align: left;
-      border-bottom: 1px solid #ddd;
-    }
-
-    th {
-      background-color: #f0f0f0;
-      font-weight: bold;
-      color: #333;
-    }
-
-    td {
-      color: #444;
-    }
-
-    tr:nth-child(even) {
-      background-color: #f9f9f9;
-    }
-  </style>
+        body {
+            background-color: #f8f9fa; /* Set your desired background color */
+        }
+        .container {
+            margin-top: 20px;
+        }
+        th {
+            font-weight: bold;
+        }
+        td, th {
+            text-align: center;
+            vertical-align: middle !important;
+        }
+        .driver-link {
+            color: #007bff; /* Set the link color */
+            text-decoration: none;
+        }
+        .driver-link:hover {
+            text-decoration: underline;
+        }
+    </style>
 </head>
 <body>
 
