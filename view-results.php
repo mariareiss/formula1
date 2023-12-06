9ddd9fc2b461cd16b96b0b606793ea83dad22f1b
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-06 12:00:52 -0600

    Update view-results.php

--- view-results.php
+++ view-results.php
@@ -1,28 +1,29 @@
 <h1>Results</h1>
 <div class="table-responsive">
-  <table class="table">
+  <table class="table" style="width: 1200px;">
     <thead>
       <tr>
-      <th>Final Position</th>
-      <th>Point Earned in this Race</th>
-      <th></th>
+        <th>Final Position</th>
+        <th>Point Earned in this Race</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
-<?php
-while ($result = $results->fetch_assoc()) {
-?>
-  <tr>
-    <td><?php echo $result['position']; ?></td>
-    <td><?php echo $result['points_earned']; ?></td>
-    <td>
-      
-    </td>
-  </tr>
-<?php
-}
-?>
+      <?php
+      while ($result = $results->fetch_assoc()) {
+      ?>
+        <tr>
+          <td><?php echo $result['position']; ?></td>
+          <td><?php echo $result['points_earned']; ?></td>
+          <td>
+            <!-- Add any additional columns or content here -->
+          </td>
+        </tr>
+      <?php
+      }
+      ?>
     </tbody>
   </table>
 </div>
+
 
