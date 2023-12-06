af57863fe371fb79aa0f0e866bb9055bd532c9ff
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-06 11:38:41 -0600

    Update view-results.php

--- view-results.php
+++ view-results.php
@@ -9,27 +9,6 @@
       </tr>
     </thead>
     <tbody>
-              body {
-            background-color: #f8f9fa; /* Set your desired background color */
-        }
-        .container {
-            margin-top: 20px;
-        }
-        th {
-            font-weight: bold;
-        }
-        td, th {
-            text-align: center;
-            vertical-align: middle !important;
-        }
-        .btn-primary {
-            background-color: #007bff; /* Set your desired button color */
-            border-color: #007bff; /* Set the button border color */
-        }
-        .btn-primary:hover {
-            background-color: #0056b3; /* Set the hover color */
-            border-color: #0056b3; /* Set the hover border color */
-        }
 <?php
 while ($result = $results->fetch_assoc()) {
 ?>
