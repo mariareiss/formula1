2b0ea901eac363b35e0662e190fcb074a6f29e5b
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-06 14:11:22 -0600

    Create model-standings-chart.php

---
+++ model-standings-chart.php
@@ -0,0 +1,15 @@
+<?php
+function selectStandings() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT d.driver_name, wins FROM Drivers d join Results rs on d.driver_id=rs.driver_id join Standing s on rs.driver_id=s.driver_id");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
