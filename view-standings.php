d61212d02e648c9397c1cd847adb8f02d5a84e57
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-06 14:24:46 -0600

    Update view-standings.php

--- view-standings.php
+++ view-standings.php
@@ -72,7 +72,7 @@
 
             <td>
       <form method="post" action="">
-        <input type="hidden" name="standing_id" value="<?php echo $standings['standing_id']; ?>">
+        <input type="hidden" name="standing_id" value="<?php echo $standing['standing_id']; ?>">
         <input type="hidden" name="actionType" value="Delete">
       <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
