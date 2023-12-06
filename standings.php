b8fb68ee6a26a71b1beec7ddaf6c3c549602eda7
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-12-06 11:49:13 -0600

    Update standings.php

--- standings.php
+++ standings.php
@@ -5,33 +5,31 @@ require_once("model-standings.php");
 $pageTitle = "Standings";
 include "view-header.php";
 
-
 if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
+  switch ($_POST['actionType']){
     case "Add":
-      if (insertStanding($_POST['wins'])) {
-        echo '<div class="alert alert-success" role="alert"> Total Wins Updated! </div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert"> Error! </div>';
-      }
-      break;
+    if (insertStandings($_POST['sSeason'], $_POST['sPoints'], $_POST['sWins'], $_POST['sPodiums'])){
+      echo '<div class="alert alert-success" role="alert">Standing added.</div>';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>';      
+    }
+    break;
     case "Edit":
-      if (updateStanding($_POST['wins'], $_POST['standing_id'])) {
-        echo '<div class="alert alert-success" role="alert"> Wins Edited! </div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert"> Error! </div>';
-      }
-      break;
-        case "Delete":
-      if (deleteStanding($_POST['standing_id'])) {
-        echo '<div class="alert alert-success" role="alert"> Wins Deleted! </div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert"> Error! </div>';
-      }
-      break;
+    if (updateStandings($_POST['sSeason'], $_POST['sPoints'], $_POST['sWins'], $_POST['sPodiums'], $_POST['stid'])){
+      echo '<div class="alert alert-success" role="alert">Standing edited.</div>';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>';      
+    }
+    break;
+    case "Delete":
+    if (deleteStandings($_POST['stid'])){
+      echo '<div class="alert alert-success" role="alert">Standing deleted.</div>';
+    } else {
+      echo '<div class="alert alert-error" role="alert">Error</div>';      
+    }
+    break;
   }
 }
-
 
 $standings = selectStandings();
 include "view-standings.php";
