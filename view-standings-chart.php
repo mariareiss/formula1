885622fb15678e6c4d458f39eb7f9b28c3f7a7f0
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-12-12 16:02:06 -0600

    Update view-standings-chart.php

--- view-standings-chart.php
+++ view-standings-chart.php
@@ -7,7 +7,7 @@
         <canvas id="myChart"></canvas>
     </div>
 
-    <div><canvas class="zdog-canvas" width="100" height="100"></canvas></div>
+    <div><canvas class="zdog-canvas" width="80" height="80"></canvas></div>
     
 </head>
 <body>
