6a5e9cc63b477fc4c93a3dfbbded33dfd8126284
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-12 14:38:01 -0600

    Update view-standings-chart.php

--- view-standings-chart.php
+++ view-standings-chart.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Standings chart</title>
-    <!-- Place your CSS or meta tags here -->
-</head>
-<body>
     <h1>Standings chart</h1>
+    
     <div>
         <canvas id="myChart"></canvas>
     </div>
 
-    <!-- Include necessary scripts -->
+    <div><canvas class="zdog-canvas" width="240" height="240"></canvas></div>
+    
+</head>
+<body>
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://unpkg.com/zdog@1/dist/zdog.dist.min.js"></script>
@@ -19,51 +19,32 @@
     <script src="https://code.highcharts.com/highcharts.js"></script>
 
     <script>
-        // Get the context of the canvas element
-        const ctx = document.getElementById('myChart').getContext('2d');
 
-        // Example data (replace it with your data retrieval logic)
-        const winsData = [
-            <?php
-            while ($standing = $standings->fetch_assoc()) {
-                echo $standing['wins'] . ", ";
-            }
-            ?>
-        ];
+        const ctx = document.getElementById('myChart');
 
-        // Example labels (replace it with your data retrieval logic)
-        const labels = [
-            <?php
-            $standings = selectStandings();
-            while ($standing = $standings->fetch_assoc()) {
-                echo "'" . $standing['standing_id'] . "', ";
-            }
-            ?>
-        ];
-
-        // Create the doughnut chart
         new Chart(ctx, {
             type: 'doughnut',
             data: {
-                datasets: [{
-                    data: [
+            datasets: [{
+                data: [
             <?php
-                        while ($standing = $standings->fetch_assoc()) {
-                            echo "'" . $standing['standing_id'] . ", ";
+                while ($standing = $standings->fetch_assoc()) {
+                    echo $standing['standing_id'] . ", ";
             }
             ?>
                 ]
                 }],
+                
                 labels: [
                     <?php
                         $standings = selectStandings();    
                         while ($standing = $standings->fetch_assoc()) {
-                            echo "'" . $standing['standing_id'] . ", ";
+                            echo "'" . $standing['standing_id'] . "', ";
             }
             ?>
-            ]
+                ]
             },
-        });
+                });
     </script>
 </body>
 </html>
