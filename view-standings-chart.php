771b2240ea4c7fe5f60849bbf8f924fb03c92121
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-12-12 15:34:56 -0600

    Update view-standings-chart.php

--- view-standings-chart.php
+++ view-standings-chart.php
@@ -8,7 +8,7 @@
 <body>
     <h1 style="text-align: center;">Standings Chart</h1>
 
-    <div style="width: 1000px; margin: 0 auto;">
+    <div style="width: 800px; margin: 0 auto;">
         <canvas id="myChart"></canvas>
     </div>
 
@@ -37,13 +37,27 @@
         ];
 
         new Chart(ctx, {
-            type: 'bar',
+            type: 'pie', // Change the chart type to 'pie'
             data: {
                 labels: labels,
                 datasets: [{
                     label: 'Standing ID',
-                    backgroundColor: 'rgba(54, 162, 235, 0.8)',
-                    borderColor: 'rgba(54, 162, 235, 1)',
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.8)',
+                        'rgba(54, 162, 235, 0.8)',
+                        'rgba(255, 206, 86, 0.8)',
+                        'rgba(75, 192, 192, 0.8)',
+                        'rgba(153, 102, 255, 0.8)',
+                        'rgba(255, 159, 64, 0.8)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
                     borderWidth: 1,
                     data: data
                 }]
@@ -56,52 +70,13 @@
                     position: 'top',
                     labels: {
                         fontColor: 'black',
-                        fontSize: 20,
+                        fontSize: 16,
                         fontStyle: 'bold'
                     }
-                },
-                scales: {
-                    xAxes: [{
-                        ticks: {
-                            fontColor: 'black',
-                            fontSize: 18,
-                            fontStyle: 'bold'
-                        },
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Drivers',
-                            fontColor: 'black',
-                            fontSize: 18,
-                            fontStyle: 'bold'
-                        }
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            fontColor: 'black',
-                            fontSize: 18,
-                            fontStyle: 'bold',
-                            beginAtZero: true
-                        },
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Points',
-                            fontColor: 'black',
-                            fontSize: 18,
-                            fontStyle: 'bold'
-                        }
-                    }]
                 }
             }
         });
     </script>
 </body>
+
 </html>
-
-
-
-
-
-
-
-
-
