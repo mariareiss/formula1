d830df932790e14ce53cadf4cc127250c111f7fe
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-12-12 14:54:58 -0600

    Update view-standings-chart.php

--- view-standings-chart.php
+++ view-standings-chart.php
@@ -1,53 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <h1>Standings chart</h1>
-    
-    <div>
+    <title>Standings Chart</title>
+</head>
+
+<body>
+    <h1>Standings Chart</h1>
+
+    <div style="width: 400px; height: 400px;">
         <canvas id="myChart"></canvas>
     </div>
 
-    <div><canvas class="zdog-canvas" width="100" height="100"></canvas></div>
-    
-</head>
-<body>
-
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <script src="https://unpkg.com/zdog@1/dist/zdog.dist.min.js"></script>
-    <script src="zdog-demo.js"></script>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
 
     <script>
+        const ctx = document.getElementById('myChart').getContext('2d');
 
-        const ctx = document.getElementById('myChart');
+        // Sample data (replace this with your actual data)
+        const driverNames = ['Hamilton', 'Verstappen', 'Bottas', 'Perez', 'Leclerc'];
+        const driverStandings = [331, 318, 203, 190, 180];
 
         new Chart(ctx, {
             type: 'doughnut',
             data: {
-            datasets: [{
-                data: [
-            <?php
-                while ($standing = $standings->fetch_assoc()) {
-                    echo $standing['standing_id'] . ", ";
-            }
-            ?>
-                ]
-                }],
-                
-                labels: [
-                    <?php
-                        $standings = selectStandings();    
-                        while ($standing = $standings->fetch_assoc()) {
-                            echo "'" . $standing['standing_id'] . "', ";
-            }
-            ?>
-                ]
+                labels: driverNames,
+                datasets: [{
+                    data: driverStandings,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.8)',
+                        'rgba(54, 162, 235, 0.8)',
+                        'rgba(255, 206, 86, 0.8)',
+                        'rgba(75, 192, 192, 0.8)',
+                        'rgba(153, 102, 255, 0.8)',
+                    ],
+                    borderWidth: 1
+                }]
             },
-                });
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    display: true,
+                    position: 'right'
+                }
+            }
+        });
     </script>
 </body>
-</html>
 
-    
-    
+</html>
