769c7704ebf356bb3a69d11ee3bf0f790829a151
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-12 14:19:04 -0600

    Update model-standings-chart.php

--- model-standings-chart.php
+++ model-standings-chart.php
@@ -2,7 +2,7 @@
 function selectStandings() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT d.driver_name, wins FROM Drivers d join Results rs on d.driver_id=rs.driver_id join Standing s on rs.driver_id=s.driver_id");
+        $stmt = $conn->prepare("SELECT d.driver_name, wins, standing_id FROM Drivers d join Results rs on d.driver_id=rs.driver_id join Standing s on rs.driver_id=s.driver_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
