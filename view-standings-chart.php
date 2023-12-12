3740964a1f566f3ab6358692ae3ffe4fd244a06e
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-12 15:49:27 -0600

    Update view-standings-chart.php

--- view-standings-chart.php
+++ view-standings-chart.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <h1>Standings chart</h1>
+    
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+
+    <div><canvas class="zdog-canvas" width="100" height="100"></canvas></div>
+    
+</head>
+<body>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://unpkg.com/zdog@1/dist/zdog.dist.min.js"></script>
+    <script src="zdog-demo.js"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+
+    <script>
+
+        const ctx = document.getElementById('myChart');
+
 new Chart(ctx, {
     type: 'pie',
     data: {
@@ -26,5 +50,8 @@ new Chart(ctx, {
         ]
     },
 });
+    </script>
+</body>
+</html>
 
 
