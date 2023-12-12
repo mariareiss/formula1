86b71f0ff5097aa64856c2d63540b754524deb61
mariareiss <142837420+mariareiss@users.noreply.github.com>
2023-12-12 15:37:24 -0600

    Update view-standings-chart.php

--- view-standings-chart.php
+++ view-standings-chart.php
@@ -1,82 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <title>Standings Chart</title>
-</head>
-
-<body>
-    <h1 style="text-align: center;">Standings Chart</h1>
-
-    <div style="width: 1100px; margin: 0 auto;">
+    <h1>Standings chart</h1>
+    
+    <div>
         <canvas id="myChart"></canvas>
     </div>
 
+    <div><canvas class="zdog-canvas" width="100" height="100"></canvas></div>
+    
+</head>
+<body>
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://unpkg.com/zdog@1/dist/zdog.dist.min.js"></script>
+    <script src="zdog-demo.js"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
 
     <script>
-        const ctx = document.getElementById('myChart').getContext('2d');
 
-        // PHP to populate JavaScript arrays from PHP data
-        const labels = [
-            <?php
-            $standings = selectStandings();
-            while ($standing = $standings->fetch_assoc()) {
-                echo "'" . $standing['standing_id'] . "', ";
-            }
-            ?>
-        ];
-
-        const data = [
-            <?php
-            $standings = selectStandings();
-            while ($standing = $standings->fetch_assoc()) {
-                echo $standing['standing_id'] . ", ";
-            }
-            ?>
-        ];
+        const ctx = document.getElementById('myChart');
 
         new Chart(ctx, {
-            type: 'pie', // Change the chart type to 'pie'
+            type: 'pie',
             data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Standing ID',
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.8)',
-                        'rgba(54, 162, 235, 0.8)',
-                        'rgba(255, 206, 86, 0.8)',
-                        'rgba(75, 192, 192, 0.8)',
-                        'rgba(153, 102, 255, 0.8)',
-                        'rgba(255, 159, 64, 0.8)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1,
-                    data: data
-                }]
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                legend: {
-                    display: true,
-                    position: 'top',
-                    labels: {
-                        fontColor: 'black',
-                        fontSize: 16,
-                        fontStyle: 'bold'
-                    }
-                }
+            datasets: [{
+                data: [
+            <?php
+                while ($standing = $standings->fetch_assoc()) {
+                    echo $standing['standing_id'] . ", ";
             }
-        });
+            ?>
+                ]
+                }],
+                
+                labels: [
+                    <?php
+                        $standings = selectStandings();    
+                        while ($standing = $standings->fetch_assoc()) {
+                            echo "'" . $standing['standing_id'] . "', ";
+            }
+            ?>
+                ]
+            },
+                });
     </script>
 </body>
-
 </html>
+
