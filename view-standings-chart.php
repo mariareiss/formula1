629478ab3d5c5c54c68fe4d9935011b6965d2da8
jordanburdette2 <111701847+jordanburdette2@users.noreply.github.com>
2023-12-12 15:44:19 -0600

    Update view-standings-chart.php

--- view-standings-chart.php
+++ view-standings-chart.php
@@ -1,51 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <h1>Standings chart</h1>
-    
-    <div>
-        <canvas id="myChart"></canvas>
-    </div>
-
-    <div><canvas class="zdog-canvas" width="100" height="100"></canvas></div>
-    
-</head>
-<body>
-
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <script src="https://unpkg.com/zdog@1/dist/zdog.dist.min.js"></script>
-    <script src="zdog-demo.js"></script>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-
-    <script>
-
-        const ctx = document.getElementById('myChart');
-
-        new Chart(ctx, {
-            type: 'pie',
-            data: {
-            datasets: [{
-                data: [
-            <?php
+new Chart(ctx, {
+    type: 'pie',
+    data: {
+        datasets: [{
+            data: [
+                <?php
                 while ($standing = $standings->fetch_assoc()) {
                     echo $standing['wins'] . ", ";
+                }
+                ?>
+            ],
+            backgroundColor: [
+                'white',   // Color for the first slice
+                'grey',    // Color for the second slice
+                'red',     // Color for the third slice
+                // Add more colors if needed for additional slices
+            ],
+        }],
+        labels: [
+            <?php
+            $standings = selectStandings();    
+            while ($standing = $standings->fetch_assoc()) {
+                echo "'" . $standing['driver_name'] . "', ";
             }
             ?>
-                ]
-                }],
-                
-                labels: [
-                    <?php
-                        $standings = selectStandings();    
-                        while ($standing = $standings->fetch_assoc()) {
-                            echo "'" . $standing['driver_name'] . "', ";
-            }
-            ?>
-                ]
-            },
-                });
-    </script>
-</body>
-</html>
+        ]
+    },
+});
+
 
