5e79498cb510fe8d9e32b57dd83a766acf08c16a
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-12 14:30:19 -0600

    Update model-builder.php

--- model-builder.php
+++ model-builder.php
@@ -12,4 +12,32 @@ function selectBuilder() {
         throw $e;
     }
 }
+
+function selectBuilderfordriver() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT driver_b_name FROM `Builder`"); 
+      $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectBuilderforteam() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT team_b_name FROM `Builder`"); 
+      $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
