757b0dcef656f1108aa6b656710b5b0d67a63611
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-13 17:51:45 -0600

    Update model-builder.php

--- model-builder.php
+++ model-builder.php
@@ -2,8 +2,8 @@
 function selectBuilder() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT driver_b_name, team_b_name, team_principal_name, team_b_season FROM `Builder`"); 
-      $stmt->execute();
+        $stmt = $conn->prepare("SELECT driver_b_name, team_b_name, team_principal_name, team_b_season FROM `Builder`");
+        $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
         return $result;
@@ -13,28 +13,42 @@ function selectBuilder() {
     }
 }
 
-function selectBuilderfordriver() {
+function insertBuilder($dbn, $dbt, $dbp, $dbs) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT driver_b_name FROM `Builder`"); 
-      $stmt->execute();
-        $result = $stmt->get_result();
+        $stmt = $conn->prepare("INSERT INTO `Builder` (driver_b_name, team_b_name, team_principal_name, team_b_season) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $dbn, $dbt, $dbp, $dbs);
+        $success = $stmt->execute();
         $conn->close();
-        return $result;
+        return $success;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
     }
 }
 
-function selectBuilderforteam() {
+function updateBuilder($dbn, $dbt, $dbp, $dbs, $dbi) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT team_b_name FROM `Builder`"); 
-      $stmt->execute();
-        $result = $stmt->get_result();
+        $stmt = $conn->prepare("UPDATE `Builder` SET `driver_b_name` = ? , `team_b_name` = ?, `team_principal_name` = ?, `team_b_season` = ? WHERE `builder_id` = ?;");
+        $stmt->bind_param("ssssi", $dbn, $dbt, $dbp, $dbs, $dbi);
+        $success = $stmt->execute();
         $conn->close();
-        return $result;
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteBuilder($dbi) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `Builder` WHERE builder_id = ?");
+        $stmt->bind_param("i", $dbi);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
