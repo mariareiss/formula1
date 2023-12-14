3afdf719a66d49d76108d65427d3e224db2bf4c9
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-14 14:46:32 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -93,6 +93,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="trivia.php">Trivia Game</a>
                         </li>
+                         <li class="nav-item">
+                            <a class="nav-link" href="insights.php">Insights</a>
+                        </li>
                     </ul>
                 </div>
             </div>
