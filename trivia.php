9de5bed20e856c19aad8b3be40811112428f3264
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-13 22:31:16 -0600

    Update trivia.php

--- trivia.php
+++ trivia.php
@@ -5,127 +5,191 @@ include "view-header.php";
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>F1 Trivia</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>F1 Trivia Page</title>
     <style>
         body {
-            font-family: 'Arial', sans-serif;
-            background-color: #f2f2f2;
+            font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
+            background-color: #f4f4f4;
         }
 
-        .container {
-            max-width: 800px;
-            margin: 0 auto;
-            padding: 20px;
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1em;
         }
 
         main {
-            margin-top: 20px;
-            background-color: #fff;
+            max-width: 800px;
+            margin: 20px auto;
             padding: 20px;
-            border-radius: 5px;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
 
-        .trivia-question {
-            display: none;
+        h2 {
+            color: #333;
         }
 
-        .options-container {
-            margin-top: 10px;
+        .question {
+            margin-bottom: 15px;
+        }
+
+        .options {
+            display: flex;
+            flex-direction: column;
         }
 
         .option {
-            cursor: pointer;
+            margin-bottom: 10px;
+        }
+
+        button {
             padding: 10px;
-            margin-bottom: 5px;
-            background-color: #fff;
-            border: 1px solid #ddd;
-            border-radius: 5px;
+            background-color: #333;
+            color: #fff;
+            border: none;
+            cursor: pointer;
+            border-radius: 4px;
         }
 
-        .option:hover {
-            background-color: #f9f9f9;
-        }
-
-        .correct {
-            background-color: #9af79a;
-        }
-
-        .incorrect {
-            background-color: #f88;
+        button:hover {
+            background-color: #555;
         }
 
         #result {
+            margin-top: 15px;
             font-weight: bold;
-            margin-top: 10px;
         }
     </style>
 </head>
-
 <body>
-    <div class="container">
-        <header>
-            <h1>F1 Trivia Game</h1>
-        </header>
 
-        <main id="trivia-main">
-            <div class="trivia-question" id="question1">
-                <h2>Question 1</h2>
-                <p>Who holds the record for the most Formula 1 World Championships?</p>
-                <div class="options-container">
-                    <div class="option" onclick="checkAnswer('question1', 'A')">A. Michael Schumacher</div>
-                    <div class="option" onclick="checkAnswer('question1', 'B')">B. Lewis Hamilton</div>
-                    <div class="option" onclick="checkAnswer('question1', 'C')">C. Ayrton Senna</div>
-                    <div class="option" onclick="checkAnswer('question1', 'D')">D. Sebastian Vettel</div>
-                </div>
-            </div>
+    <header>
+        <h1>F1 Trivia Page</h1>
+    </header>
 
+    <main>
+        <h2>Trivia Question</h2>
+        <div id="question" class="question"></div>
+        
+        <div class="options" id="options"></div>
 
-            <div id="result"></div>
-        </main>
+        <button onclick="checkAnswer()">Submit Answer</button>
+        <p id="result"></p>
+    </main>
 
-        <script>
-            var currentQuestion = 1;
-
-            function checkAnswer(questionId, selectedOption) {
-                var question = document.getElementById(questionId);
-                var options = question.getElementsByClassName('option');
-                var result = document.getElementById('result');
-
-                for (var i = 0; i < options.length; i++) {
-                    options[i].style.backgroundColor = "#fff"; // Reset background color
-                }
-
-                if (selectedOption === 'B') {
-                    question.style.display = "none";
-                    currentQuestion++;
-
-                    if (currentQuestion <= 10) {
-                        var nextQuestion = document.getElementById('question' + currentQuestion);
-                        nextQuestion.style.display = "block";
-                    } else {
-                        result.innerHTML = "Congratulations! You've completed the trivia.";
-                    }
-                } else {
-                    options.forEach(function (option) {
-                        if (option.innerHTML.includes(selectedOption)) {
-                            option.style.backgroundColor = "#f88"; // Red for incorrect answer
-                        } else if (option.innerHTML.includes('B')) {
-                            option.style.backgroundColor = "#9af79a"; // Green for correct answer
-                        }
-                    });
-                    result.innerHTML = "Incorrect! Try again.";
-                }
+    <script>
+        const triviaData = [
+            {
+                question: "Which team has the most Constructors' Championships in Formula 1?",
+                options: ["Ferrari", "Mercedes", "Red Bull Racing", "McLaren"],
+                correctAnswer: "Ferrari"
+            },
+            {
+                question: "Who holds the record for the most Grand Prix wins?",
+                options: ["Michael Schumacher", "Lewis Hamilton", "Ayrton Senna", "Alain Prost"],
+                correctAnswer: "Lewis Hamilton"
+            },
+            {
+                question: "In which year did Formula 1 have its first race?",
+                options: ["1950", "1960", "1970", "1980"],
+                correctAnswer: "1950"
+            },
+            {
+                question: "Which driver has won the most World Championships?",
+                options: ["Ayrton Senna", "Juan Manuel Fangio", "Michael Schumacher", "Lewis Hamilton"],
+                correctAnswer: "Lewis Hamilton"
+            },
+            {
+                question: "What is the official tire supplier for Formula 1?",
+                options: ["Michelin", "Pirelli", "Bridgestone", "Goodyear"],
+                correctAnswer: "Pirelli"
+            },
+            {
+                question: "Which circuit is known as 'The Temple of Speed'?",
+                options: ["Monaco", "Silverstone", "Monza", "Spa-Francorchamps"],
+                correctAnswer: "Monza"
+            },
+            {
+                question: "How many teams participate in a typical Formula 1 season?",
+                options: ["8", "10", "12", "20"],
+                correctAnswer: "10"
+            },
+            {
+                question: "Which country has produced the most Formula 1 World Champions?",
+                options: ["United Kingdom", "Germany", "Brazil", "Italy"],
+                correctAnswer: "United Kingdom"
+            },
+            {
+                question: "Who is the youngest Formula 1 World Champion?",
+                options: ["Sebastian Vettel", "Ayrton Senna", "Michael Schumacher", "Lewis Hamilton"],
+                correctAnswer: "Sebastian Vettel"
+            },
+            {
+                question: "What is DRS in Formula 1?",
+                options: ["Drag Reduction System", "Dynamic Race Strategy", "Downforce Recovery System", "Double Race Suspension"],
+                correctAnswer: "Drag Reduction System"
             }
-        </script>
-    </div>
-</body>
+            // Add more trivia questions as needed
+        ];
 
+        let currentQuestionIndex = 0;
+        const questionElement = document.getElementById("question");
+        const optionsElement = document.getElementById("options");
+        const resultElement = document.getElementById("result");
+
+        function loadQuestion() {
+            const currentQuestion = triviaData[currentQuestionIndex];
+            questionElement.textContent = currentQuestion.question;
+
+            optionsElement.innerHTML = "";
+            currentQuestion.options.forEach((option, index) => {
+                const optionElement = document.createElement("div");
+                optionElement.className = "option";
+                optionElement.innerHTML = `<input type="radio" name="answer" value="${option}" id="option${index + 1}">
+                                            <label for="option${index + 1}">${option}</label>`;
+                optionsElement.appendChild(optionElement);
+            });
+        }
+
+        function checkAnswer() {
+            const selectedOption = document.querySelector('input[name="answer"]:checked');
+            if (selectedOption) {
+                const userAnswer = selectedOption.value;
+                const correctAnswer = triviaData[currentQuestionIndex].correctAnswer;
+
+                if (userAnswer === correctAnswer) {
+                    resultElement.textContent = "Correct!";
+                } else {
+                    resultElement.textContent = `Wrong! The correct answer is ${correctAnswer}.`;
+                }
+
+                currentQuestionIndex++;
+                if (currentQuestionIndex < triviaData.length) {
+                    setTimeout(() => {
+                        resultElement.textContent = "";
+                        loadQuestion();
+                    }, 1500);
+                } else {
+                    resultElement.textContent = "Quiz completed!";
+                }
+            } else {
+                resultElement.textContent = "Please select an answer.";
+            }
+        }
+
+        // Initial load
+        loadQuestion();
+    </script>
+
+</body>
 </html>
 
 
