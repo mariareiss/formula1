230c754a7a9b6a75e6d31de5f0da038399dae978
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-13 22:20:10 -0600

    Update trivia.php

--- trivia.php
+++ trivia.php
@@ -187,39 +187,40 @@ include "view-header.php";
         </main>
 
 <script>
-    var currentQuestion = 1;
+  var currentQuestion = 1;
 
-    function checkAnswer(questionId, selectedOption) {
-        var question = document.getElementById(questionId);
-        var options = question.getElementsByClassName('option');
-        var result = document.getElementById('result');
+  function checkAnswer(questionId, selectedOption) {
+    var question = document.getElementById(questionId);
+    var options = question.getElementsByClassName('option');
+    var result = document.getElementById('result');
 
-        for (var i = 0; i < options.length; i++) {
-            options[i].style.backgroundColor = "#fff"; // Reset background color
-        }
-
-        if (selectedOption === 'B') {
-            question.style.display = "none";
-            currentQuestion++;
-
-            if (currentQuestion <= 10) {
-                var nextQuestion = document.getElementById('question' + currentQuestion);
-                nextQuestion.style.display = "block";
-            } else {
-                result.innerHTML = "Congratulations! You've completed the trivia.";
-            }
-        } else {
-            options.forEach(function (option) {
-                if (option.innerHTML.includes(selectedOption)) {
-                    option.style.backgroundColor = "#f88"; // Red for incorrect answer
-                } else if (option.innerHTML.includes('B')) {
-                    option.style.backgroundColor = "#9af79a"; // Green for correct answer
-                }
-            });
-            result.innerHTML = "Incorrect! Try again.";
-        }
+    for (var i = 0; i < options.length; i++) {
+      options[i].style.backgroundColor = "#fff"; // Reset background color
     }
+
+    if (selectedOption === 'B') {
+      question.style.display = "none";
+      currentQuestion++;
+
+      if (currentQuestion <= 10) {
+        var nextQuestion = document.getElementById('question' + currentQuestion);
+        nextQuestion.style.display = "block";
+      } else {
+        result.innerHTML = "Congratulations! You've completed the trivia.";
+      }
+    } else {
+      options.forEach(function (option) {
+        if (option.innerHTML.includes(selectedOption)) {
+          option.style.backgroundColor = "#f88"; // Red for incorrect answer
+        } else if (option.innerHTML.includes('B')) {
+          option.style.backgroundColor = "#9af79a"; // Green for correct answer
+        }
+      });
+      result.innerHTML = "Incorrect! Try again.";
+    }
+  }
 </script>
+
 
     </div>
 </body>
