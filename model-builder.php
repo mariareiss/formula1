a4d7a42d1529b2f2368d48a5f34a8864e923117a
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-14 11:22:17 -0600

    Update model-builder.php

--- model-builder.php
+++ model-builder.php
@@ -2,7 +2,7 @@
 function selectBuilder() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT driver_b_name, team_b_name, team_principal_name, team_b_season FROM `Builder`");
+        $stmt = $conn->prepare("SELECT team_builder_id, driver_b_name, team_b_name, team_principal_name, team_b_season FROM `Builder`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
