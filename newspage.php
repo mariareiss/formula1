01d6c4ee951fa485ebad8c7d4e5b575a2219cbc1
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-14 21:35:51 -0600

    Update newspage.php

--- newspage.php
+++ newspage.php
@@ -17,12 +17,6 @@ include "view-header.php";
             padding: 0;
             background-color: #f8f8f8;
             color: #333;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            overflow: hidden;
         }
 
         header {
@@ -31,7 +25,6 @@ include "view-header.php";
             background-color: #343a40;
             color: #fff;
             border-bottom: 2px solid #fff;
-            width: 100%;
         }
 
         h1 {
@@ -113,45 +106,7 @@ include "view-header.php";
                 <p class="team-name">Mercedes</p>
             </div>
 
-            <div class="grid-item" onclick="redirectToWebsite('https://www.redbullracing.com/int-en')">
-                <div class="team-logo"><img src="redbull.jpg" alt="Red Bull Logo"></div>
-                <p class="team-name">Red Bull</p>
-            </div>
-
-            <div class="grid-item" onclick="redirectToWebsite('https://www.ferrari.com/en-EN/formula1')">
-                <div class="team-logo"><img src="ferrari.jpg" alt="Ferrari Logo"></div>
-                <p class="team-name">Ferrari</p>
-            </div>
-
-            <div class="grid-item" onclick="redirectToWebsite('https://www.mclaren.com/racing/')">
-                <div class="team-logo"><img src="mclaren2.png" alt="McLaren Logo"></div>
-                <p>McLaren</p>
-            </div>
-
-            <div class="grid-item" onclick="redirectToWebsite('https://www.alpine-cars.co.uk/formula-1/f1-team.html')">
-                <div class="team-logo"><img src="alpine.jpg" alt="Alpine Logo"></div>
-                <p>Alpine</p>
-            </div>
-
-            <div class="grid-item" onclick="redirectToWebsite('https://www.astonmartinf1.com/en-GB')">
-                <div class="team-logo"><img src="aston.jpg" alt="Aston Martin Logo"></div>
-                <p>Aston Martin</p>
-            </div>
-
-            <div class="grid-item" onclick="redirectToWebsite('https://scuderia.alphatauri.com/en/')">
-                <div class="team-logo"><img src="Alfa.png" alt="Alfa Romeo Logo"></div>
-                <p>Alfa Romeo</p>
-            </div>
-
-            <div class="grid-item" onclick="redirectToWebsite('	https://www.haasf1team.com/')">
-                <div class="team-logo"><img src="haas.png" alt="Haas Logo"></div>
-                <p>Haas</p>
-            </div>
-
-            <div class="grid-item" onclick="redirectToWebsite('	https://www.williamsf1.com/')">
-                <div class="team-logo"><img src="williams.png" alt="Williams Logo"></div>
-                <p>Williams</p>
-            </div>
+            <!-- ... (other team items) ... -->
 
             <div class="grid-item" onclick="redirectToWebsite('https://scuderia.alphatauri.com/en/')">
                 <div class="team-logo"><img src="alpha.png" alt="AlphaTauri Logo"></div>
