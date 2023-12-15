5dacef93a695ab299ee2f91017ba40857ab51902
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-15 00:19:28 -0600

    Update view-standings.php

--- view-standings.php
+++ view-standings.php
@@ -1,20 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-<div class="container">
-    <h1>Standings</h1>
-    <div class="col-auto">
-  <?php
-  include "view-standings-newform.php";
-  ?>
-    </div>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Standing</title>
+    <title>Standings</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         body {
             background-color: #f8f9fa; /* Set your desired background color */
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            text-align: center;
+            padding: 20px;
+            background-color: #001f3f; /* Dark blue color */
+            color: #fff;
+            border-bottom: 2px solid #fff;
+        }
+        h1 {
+            margin: 0;
+            font-size: 28px;
         }
         .container {
             margin-top: 20px;
@@ -37,57 +46,76 @@
             text-decoration: underline;
         }
         .table {
-            margin-top: 05px; /* Add margin to the table */
+            margin-top: 5px; /* Add margin to the table */
+        }
+        .col-auto {
+            margin-bottom: 20px;
+        }
+        .btn-primary {
+            background-color: #007bff; /* Set your desired button color */
+            border-color: #007bff; /* Set the button border color */
+        }
+        .btn-primary:hover {
+            background-color: #0056b3; /* Set the hover color */
+            border-color: #0056b3; /* Set the hover border color */
         }
     </style>
 </head>
+
 <body>
-    
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Driver</th>
-                <th>Season</th>
-                <th>Points</th>
-                <th>Wins</th>
-                <th>Podiums</th>
-                <th></th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php while ($standing = $standings->fetch_assoc()) { ?>
-                <tr>
-                    <td><?php echo $standing['driver_name']; ?></td>
-                    <td><?php echo $standing['season']; ?></td>
-                    <td><?php echo $standing['points']; ?></td>
-                    <td><?php echo $standing['wins']; ?></td>
-                    <td><?php echo $standing['podiums']; ?></td>
-                    <td>
-                        <?php
-                          include "view-standings-editform.php";
-                          ?>
-                    </td>
 
-            <td>
-      <form method="post" action="">
-        <input type="hidden" name="standing_id" value="<?php echo $standing['standing_id']; ?>">
-        <input type="hidden" name="actionType" value="Delete">
-      <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-          <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-          </svg>
-      </button>
-    </form>
-    </td>            
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
+    <div class="container">
+        <header>
+            <h1>Formula One Standings Data</h1>
+        </header>
+        <div class="col-auto">
+            <?php include "view-standings-newform.php"; ?>
+        </div>
+
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Driver</th>
+                        <th>Season</th>
+                        <th>Points</th>
+                        <th>Wins</th>
+                        <th>Podiums</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($standing = $standings->fetch_assoc()) { ?>
+                        <tr>
+                            <td><?php echo $standing['driver_name']; ?></td>
+                            <td><?php echo $standing['season']; ?></td>
+                            <td><?php echo $standing['points']; ?></td>
+                            <td><?php echo $standing['wins']; ?></td>
+                            <td><?php echo $standing['podiums']; ?></td>
+                            <td>
+                                <?php include "view-standings-editform.php"; ?>
+                            </td>
+                            <td>
+                                <form method="post" action="">
+                                    <input type="hidden" name="standing_id" value="<?php echo $standing['standing_id']; ?>">
+                                    <input type="hidden" name="actionType" value="Delete">
+                                    <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.500 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                                        </svg>
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 
+    <!-- Bootstrap JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+
 </html>
