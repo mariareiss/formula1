9359a45751103a789a5e4fb21ce29fc61c61210e
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-15 01:20:58 -0600

    Update game.php

--- game.php
+++ game.php
@@ -12,13 +12,11 @@ include "view-header.php";
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <style>
     body {
-      background-image: url('f1-image.avif');
-      background-size: cover;
-      background-repeat: no-repeat;
-      opacity: 1.0;
-      margin: 0;
-      padding: 0;
-    }
+          background-color: #f8f9fa; /* Set your desired background color */
+          font-family: 'Arial', sans-serif;
+          margin: 0;
+          padding: 0;
+      }
 
     header {
       text-align: center;
