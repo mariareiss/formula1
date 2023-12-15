ff1d377e50dbd71be799b77b53253c5200c44b6f
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-15 01:02:09 -0600

    Update game.php

--- game.php
+++ game.php
@@ -5,20 +5,32 @@ include "view-header.php";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <title>Formula 1 Race Simulation</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <style>
     body {
       background-image: url('f1-image.avif');
       background-size: cover;
       background-repeat: no-repeat;
       opacity: 1.0;
+      margin: 0;
+      padding: 0;
     }
 
- h1 {
+    header {
       text-align: center;
-      margin-top: 20px;
+      padding: 20px;
+      background-color: #001f3f; /* Dark blue color */
+      color: #fff;
+      border-bottom: 2px solid #fff;
+    }
+
+    h1 {
+      margin: 0;
+      font-size: 28px;
     }
 
     .container {
@@ -87,174 +99,59 @@ include "view-header.php";
     }
   </style>
 </head>
+
 <body>
+  <header>
+    <h1>Formula 1 Race Simulation</h1>
+  </header>
 
-<h1>Formula 1 Race Simulation</h1>
+  <div class="container">
+    <div>
+      <p>Select pilots:</p>
+      <ul id="pilots">
+        <li>
+          <input type="checkbox" id="redCheckbox" onchange="selectPilot('red', this.checked)">
+          <label for="redCheckbox">Lewis Hamilton</label>
+        </li>
+        <li>
+          <input type="checkbox" id="blueCheckbox" onchange="selectPilot('blue', this.checked)">
+          <label for="blueCheckbox">Max Verstappen</label>
+        </li>
+        <li>
+          <input type="checkbox" id="greenCheckbox" onchange="selectPilot('green', this.checked)">
+          <label for="greenCheckbox">Charles Leclerc</label>
+        </li>
+      </ul>
+    </div>
 
-<div class="container">
-  <div>
-    <p>Select pilots:</p>
-    <ul id="pilots">
-      <li>
-        <input type="checkbox" id="redCheckbox" onchange="selectPilot('red', this.checked)">
-        <label for="redCheckbox">Lewis Hamilton</label>
-      </li>
-      <li>
-        <input type="checkbox" id="blueCheckbox" onchange="selectPilot('blue', this.checked)">
-        <label for="blueCheckbox">Max Verstappen</label>
-      </li>
-      <li>
-        <input type="checkbox" id="greenCheckbox" onchange="selectPilot('green', this.checked)">
-        <label for="greenCheckbox">Charles Leclerc</label>
-      </li>
-    </ul>
+    <div>
+      <p>Set speed for each pilot:</p>
+      <input type="range" id="speedRed" min="1" max="50" value="25" onchange="setSpeed('red', this.value)">
+      <label for="speedRed">Lewis Hamilton</label>
+      <br>
+      <input type="range" id="speedBlue" min="1" max="50" value="25" onchange="setSpeed('blue', this.value)">
+      <label for="speedBlue">Max Verstappen</label>
+      <br>
+      <input type="range" id="speedGreen" min="1" max="50" value="25" onchange="setSpeed('green', this.value)">
+      <label for="speedGreen">Charles Leclerc</label>
+      <!-- Add more speed controls for other pilots here -->
+    </div>
+
+    <div class="track" id="raceTrack">
+      <div class="car" id="redCar" onmousedown="startDrag(event, 'red')"><span class="car-text">Lewis Hamilton</span></div>
+      <div class="car" id="blueCar" onmousedown="startDrag(event, 'blue')"><span class="car-text">Max Verstappen</span></div>
+      <div class="car" id="greenCar" onmousedown="startDrag(event, 'green')"><span class="car-text">Charles Leclerc</span></div>
+    </div>
+
+    <button onclick="startRace()">Start Race</button>
+    <button onclick="stopRace()">Stop Race</button>
+    <button onclick="restartRace()">Restart Race</button>
   </div>
 
-   <div>
-    <p>Set speed for each pilot:</p>
-    <input type="range" id="speedRed" min="1" max="50" value="25" onchange="setSpeed('red', this.value)">
-    <label for="speedRed">Lewis Hamilton</label>
-    <br>
-    <input type="range" id="speedBlue" min="1" max="50" value="25" onchange="setSpeed('blue', this.value)">
-    <label for="speedBlue">Max Verstappen</label>
-    <br>
-    <input type="range" id="speedGreen" min="1" max="50" value="25" onchange="setSpeed('green', this.value)">
-    <label for="speedGreen">Charles Leclerc</label>
-    <!-- Add more speed controls for other pilots here -->
-  </div>
-
-  <div class="track" id="raceTrack">
-    <div class="car" id="redCar" onmousedown="startDrag(event, 'red')"><span class="car-text">Lewis Hamilton</span></div>
-    <div class="car" id="blueCar" onmousedown="startDrag(event, 'blue')"><span class="car-text">Max Verstappen</span></div>
-    <div class="car" id="greenCar" onmousedown="startDrag(event, 'green')"><span class="car-text">Charles Leclerc</span></div>
-  </div>
-
-  <button onclick="startRace()">Start Race</button>
-  <button onclick="stopRace()">Stop Race</button>
-  <button onclick="restartRace()">Restart Race</button>
-</div>
-
-<script>
-  let selectedPilot = '';
-
-  function selectPilot(color, isChecked) {
-    if (isChecked) {
-      selectedPilot = color;
-    } else {
-      selectedPilot = ''; // Unselect if checkbox is unchecked
-    }
-  }
-
-  function setSpeed(color, value) {
-    pilots[color].speed = value;
-  }
-
-  let pilots = {
-    'red': { 'name': 'Lewis Hamilton', 'speed': 25, 'intervalId': null },
-    'blue': { 'name': 'Max Verstappen', 'speed': 25, 'intervalId': null },
-    'green': { 'name': 'Charles Leclerc', 'speed': 25, 'intervalId': null }
-    // Add more pilots here
-  };
-
-  function startRace() {
-    if (!selectedPilot) {
-      alert('Please select a pilot before starting the race!');
-      return;
-    }
-
-    const raceTrack = document.getElementById('raceTrack');
-    const redCar = document.getElementById('redCar');
-    const blueCar = document.getElementById('blueCar');
-    const greenCar = document.getElementById('greenCar');
-
-    redCar.style.backgroundColor = 'red';
-    blueCar.style.backgroundColor = 'blue';
-    greenCar.style.backgroundColor = 'green';
-
-    const distance = 550;
-
-    const moveIntervalRed = setInterval(() => {
-      const currentPositionRed = parseInt(redCar.style.left, 10) || 0;
-      const newPositionRed = currentPositionRed + pilots['red'].speed / 2;
-
-      if (newPositionRed >= distance) {
-        clearInterval(moveIntervalRed);
-        redCar.style.left = `${distance}px`;
-      } else {
-        redCar.style.left = `${newPositionRed}px`;
-      }
-    }, 100);
-
-    const moveIntervalBlue = setInterval(() => {
-      const currentPositionBlue = parseInt(blueCar.style.left, 10) || 0;
-      const newPositionBlue = currentPositionBlue + pilots['blue'].speed / 2;
-
-      if (newPositionBlue >= distance) {
-        clearInterval(moveIntervalBlue);
-        blueCar.style.left = `${distance}px`;
-      } else {
-        blueCar.style.left = `${newPositionBlue}px`;
-      }
-    }, 100);
-     const moveIntervalGreen = setInterval(() => {
-      const currentPositionGreen = parseInt(greenCar.style.left, 10) || 0;
-      const newPositionGreen = currentPositionGreen + pilots['green'].speed / 2;
-
-      if (newPositionGreen >= distance) {
-        clearInterval(moveIntervalGreen);
-        greenCar.style.left = `${distance}px`;
-      } else {
-        greenCar.style.left = `${newPositionGreen}px`;
-      }
-    }, 100);
-
-    pilots['red'].intervalId = moveIntervalRed;
-    pilots['blue'].intervalId = moveIntervalBlue;
-    pilots['green'].intervalId = moveIntervalGreen;
-  }
-
-  function stopRace() {
-    clearInterval(pilots['red'].intervalId);
-    clearInterval(pilots['blue'].intervalId);
-    clearInterval(pilots['green'].intervalId);
-  }
-
-  function restartRace() {
-    stopRace();
-    const redCar = document.getElementById('redCar');
-    const blueCar = document.getElementById('blueCar');
-    const greenCar = document.getElementById('greenCar');
-    redCar.style.left = '0px';
-    blueCar.style.left = '0px';
-    greenCar.style.left = '0px';
-  }
-
-  let isDragging = false;
-  let offsetX = 0;
-
-  function startDrag(event, color) {
-    isDragging = true;
-    offsetX = event.clientX - parseInt(document.getElementById(color + 'Car').style.left || 0);
-    document.addEventListener('mousemove', e => moveCar(e, color));
-    document.addEventListener('mouseup', stopDrag);
-  }
-
-  function moveCar(event, color) {
-    if (isDragging) {
-      const car = document.getElementById(color + 'Car');
-      car.style.left = `${event.clientX - offsetX}px`;
-    }
-  }
-
-  function stopDrag() {
-    isDragging = false;
-    document.removeEventListener('mousemove', moveCar);
-    document.removeEventListener('mouseup',
-    stopDrag);
-  }
-</script>
-
+  <!-- Bootstrap JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+
 </html>
 
 <?php
