8b29b4cb2961dedefe5b759e759e5ca2eeb3085c
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-14 20:05:32 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -8,42 +8,49 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
-        
-body {
-    background: linear-gradient(45deg, #aed9e0, #ffb6c1); /* Pastel-themed gradient background */
-    margin: 0;
-    padding: 0;
-    font-family: 'Arial', sans-serif;
-}
-
+        body {
+            background: linear-gradient(45deg, #aed9e0, #ffb6c1);
+            margin: 0;
+            padding: 0;
+            font-family: 'Arial', sans-serif;
+        }
 
         .navbar {
-            background: linear-gradient(to right, #dc3545, #8b0000); /* Gradient background */
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Subtle box shadow */
+            background: linear-gradient(to right, #dc3545, #8b0000);
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
             max-width: 100%;
             z-index: 1000;
         }
 
         .navbar-brand {
-            color: #fff; /* White text color */
+            color: #fff;
             font-weight: bold;
         }
 
         .navbar-nav .nav-link {
-            color: #fff; /* White text color */
+            color: #fff;
             transition: color 0.3s ease;
         }
 
         .navbar-nav .nav-link:hover {
-            color: #ffc107; /* Yellow on hover */
+            color: #ffc107;
         }
 
         .navbar-nav .nav-link.active {
-            color: #000; /* Black text color for active link */
+            color: #000;
             font-weight: bold;
-            background-color: #ffc107; /* Yellow background for active link */
+            background-color: #ffc107;
             border-radius: 5px;
             padding: 8px 15px;
+        }
+
+        /* Add new styles for the yellow box and black text */
+        .navbar-nav .nav-link.f1-hub {
+            background-color: #ffc107;
+            border-radius: 5px;
+            padding: 8px 15px;
+            color: #000;
+            font-weight: bold;
         }
 
         .search-container {
@@ -58,19 +65,19 @@ body {
             border: none;
             font-size: 17px;
             max-width: 100%;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Subtle box shadow */
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         }
 
         .search-container button {
             padding: 10px;
             margin: 5px 0;
             cursor: pointer;
-            background: #007bff; /* Blue button color */
+            background: #007bff;
             color: white;
             border: none;
             font-size: 17px;
             border-radius: 5px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Subtle box shadow */
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         }
     </style>
 </head>
@@ -79,7 +86,7 @@ body {
     <div class="container">
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Formula 1 Hub</a>
+                <a class="navbar-brand" href="#">F1 News Hub</a> <!-- Renamed the tab -->
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
