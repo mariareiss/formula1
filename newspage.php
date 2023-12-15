81bb167df84d3b83cb8b27cd17da0032cf1addfa
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-14 19:29:47 -0600

    Update newspage.php

--- newspage.php
+++ newspage.php
@@ -5,153 +5,155 @@ include "view-header.php";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <style>
-    body {
-      font-family: 'Arial', sans-serif;
-      margin: 0;
-      padding: 0;
-      background-color: #f8f8f8;
-      color: #333;
-    }
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f8f8f8;
+            color: #333;
+        }
 
-    header {
-      text-align: center;
-      padding: 10px;
-      max-width: 100%;
-      display: flex;
-      flex-direction: column; 
-      align-items: center;
-    }
+        header {
+            text-align: center;
+            padding: 10px;
+            max-width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            background: linear-gradient(to right, #dc3545, #8b0000); /* Gradient background */
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Subtle box shadow */
+        }
 
-    h1 {
-      margin: 0;
-      color: #fff;
-      text-align: center;
-      max-width: 100%;
-      display: flex;
-      flex-direction: column; /* Stack items vertically */
-      align-items: center;
-    }
+        h1 {
+            margin: 0;
+            color: #fff;
+            text-align: center;
+            max-width: 100%;
+            display: flex;
+            flex-direction: column; /* Stack items vertically */
+            align-items: center;
+        }
 
-    section {
-      margin: 50px;
-    }
+        section {
+            margin: 50px;
+        }
 
-    .grid-container {
-      display: grid;
-      gap: 20px;
-      grid-template-columns: repeat(2, 1fr);
-      justify-content: center;
-    }
+        .grid-container {
+            display: grid;
+            gap: 20px;
+            grid-template-columns: repeat(2, 1fr);
+            justify-content: center;
+        }
 
-    .grid-item {
-      text-align: center;
-      padding: 20px;
-      background-color: #f2f2f2;
-      border-radius: 10px;
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-      cursor: pointer;
-      transition: transform 0.2s ease-in-out;
-      display: flex;
-      flex-direction: column; /* Stack items vertically */
-      align-items: center; /* Center items horizontally */
-    }
+        .grid-item {
+            text-align: center;
+            padding: 20px;
+            background-color: #f2f2f2;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            cursor: pointer;
+            transition: transform 0.2s ease-in-out;
+            display: flex;
+            flex-direction: column; /* Stack items vertically */
+            align-items: center; /* Center items horizontally */
+        }
 
-    .grid-item:hover {
-      transform: scale(1.05);
-    }
-    
-    .team-logo {
-      width: 200px;
-      height: 200px;
-      object-fit: cover;
-      border: 5px solid #fff;
-      border-radius: 10px;
-      overflow: hidden;
-      margin-bottom: 10px; /* Add margin to create space between image and text */
-    }
+        .grid-item:hover {
+            transform: scale(1.05);
+        }
 
-    
-  </style>
-  <title>Formula One News Hub</title>
+        .team-logo {
+            width: 150px; /* Adjusted size for better visual appeal */
+            height: 150px; /* Adjusted size for better visual appeal */
+            object-fit: cover;
+            border: 5px solid #fff;
+            border-radius: 10px;
+            overflow: hidden;
+            margin-bottom: 10px; /* Add margin to create space between image and text */
+            display: flex;
+            justify-content: center; /* Center image horizontally within the container */
+            align-items: center; /* Center image vertically within the container */
+        }
+    </style>
+    <title>Formula One News Hub</title>
 </head>
+
 <body>
-  <header>
-    <h1>Formula One News Hub</h1>
-  </header>
-  
-  <section id="byTeam">
-    <h2>Click on a team to learn more</h2>
-    <div class="grid-container">
-      <!-- Team grid with team names -->
-      
-<div class="grid-item" onclick="redirectToWebsite('https://www.mercedesamgf1.com/')">
-  <div class="team-logo"><img src="mercedes_logo.png" alt="Mercedes Logo"></div>
-  <p>Mercedes</p>
-</div>
+    <header>
+        <h1>Formula One News Hub</h1>
+    </header>
 
-<div class="grid-item" onclick="redirectToWebsite('https://www.redbullracing.com/int-en')">
-  <div class="team-logo"><img src="redbull.jpg" alt="Red Bull Logo"></div>
-  <p>Red Bull</p>
-</div>
+    <section id="byTeam">
+        <h2>Click on a team to learn more</h2>
+        <div class="grid-container">
+            <!-- Team grid with team names -->
+            <div class="grid-item" onclick="redirectToWebsite('https://www.mercedesamgf1.com/')">
+                <div class="team-logo"><img src="mercedes_logo.png" alt="Mercedes Logo"></div>
+                <p>Mercedes</p>
+            </div>
 
-<div class="grid-item" onclick="redirectToWebsite('https://www.ferrari.com/en-EN/formula1')">
-  <div class="team-logo"><img src="ferrari.jpg" alt="Ferrari Logo"></div>
-  <p>Ferrari</p>
-</div>
+            <div class="grid-item" onclick="redirectToWebsite('https://www.redbullracing.com/int-en')">
+                <div class="team-logo"><img src="redbull.jpg" alt="Red Bull Logo"></div>
+                <p>Red Bull</p>
+            </div>
 
-<div class="grid-item" onclick="redirectToWebsite('https://www.mclaren.com/racing/')">
-  <div class="team-logo"><img src="mclaren2.png" alt="McLaren Logo"></div>
-  <p>McLaren</p>
-</div>
+            <div class="grid-item" onclick="redirectToWebsite('https://www.ferrari.com/en-EN/formula1')">
+                <div class="team-logo"><img src="ferrari.jpg" alt="Ferrari Logo"></div>
+                <p>Ferrari</p>
+            </div>
 
-<div class="grid-item" onclick="redirectToWebsite('https://www.alpine-cars.co.uk/formula-1/f1-team.html')">
-  <div class="team-logo"><img src="alpine.jpg" alt="Alpine Logo"></div>
-  <p>Alpine</p>
-</div>
+            <div class="grid-item" onclick="redirectToWebsite('https://www.mclaren.com/racing/')">
+                <div class="team-logo"><img src="mclaren2.png" alt="McLaren Logo"></div>
+                <p>McLaren</p>
+            </div>
 
-<div class="grid-item" onclick="redirectToWebsite('https://www.astonmartinf1.com/en-GB')">
-  <div class="team-logo"><img src="aston.jpg" alt="Aston Martin Logo"></div>
-  <p>Aston Martin</p>
-</div>
+            <div class="grid-item" onclick="redirectToWebsite('https://www.alpine-cars.co.uk/formula-1/f1-team.html')">
+                <div class="team-logo"><img src="alpine.jpg" alt="Alpine Logo"></div>
+                <p>Alpine</p>
+            </div>
 
-<div class="grid-item" onclick="redirectToWebsite('https://scuderia.alphatauri.com/en/')">
-  <div class="team-logo"><img src="Alfa.png" alt="Alfa Romeo Logo"></div>
-  <p>Alfa Romeo</p>
-</div>
+            <div class="grid-item" onclick="redirectToWebsite('https://www.astonmartinf1.com/en-GB')">
+                <div class="team-logo"><img src="aston.jpg" alt="Aston Martin Logo"></div>
+                <p>Aston Martin</p>
+            </div>
 
-<div class="grid-item" onclick="redirectToWebsite('	https://www.haasf1team.com/')">
-  <div class="team-logo"><img src="haas.png" alt="Haas Logo"></div>
-  <p>Haas</p>
-</div>
+            <div class="grid-item" onclick="redirectToWebsite('https://scuderia.alphatauri.com/en/')">
+                <div class="team-logo"><img src="Alfa.png" alt="Alfa Romeo Logo"></div>
+                <p>Alfa Romeo</p>
+            </div>
 
-<div class="grid-item" onclick="redirectToWebsite('	https://www.williamsf1.com/')">
-  <div class="team-logo"><img src="williams.png" alt="Williams Logo"></div>
-  <p>Williams</p>
-</div>
+            <div class="grid-item" onclick="redirectToWebsite('	https://www.haasf1team.com/')">
+                <div class="team-logo"><img src="haas.png" alt="Haas Logo"></div>
+                <p>Haas</p>
+            </div>
 
-<div class="grid-item" onclick="redirectToWebsite('https://scuderia.alphatauri.com/en/')">
-  <div class="team-logo"><img src="alpha.png" alt="AlphaTauri Logo"></div>
-  <p>AlphaTauri</p>
-</div>
+            <div class="grid-item" onclick="redirectToWebsite('	https://www.williamsf1.com/')">
+                <div class="team-logo"><img src="williams.png" alt="Williams Logo"></div>
+                <p>Williams</p>
+            </div>
 
-    </div>
-  </section>
+            <div class="grid-item" onclick="redirectToWebsite('https://scuderia.alphatauri.com/en/')">
+                <div class="team-logo"><img src="alpha.png" alt="AlphaTauri Logo"></div>
+                <p>AlphaTauri</p>
+            </div>
+        </div>
+    </section>
 
-
-<script>
-    function redirectToWebsite(url) {
-        window.open(url, '_blank');
-    }
-</script>
+    <script>
+        function redirectToWebsite(url) {
+            window.open(url, '_blank');
+        }
+    </script>
 
 </body>
+
 </html>
 
 <?php
 include "view-footer.php";
 ?>
-
