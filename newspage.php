badd64df9a2d87e9eedd5d99a896dafbe8ba8102
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-14 20:01:16 -0600

    Update newspage.php

--- newspage.php
+++ newspage.php
@@ -37,7 +37,7 @@ include "view-header.php";
         .grid-container {
             display: grid;
             gap: 20px;
-            grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+            grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
             justify-content: center;
         }
         
