a1faa85e9c5f7374c6a62c3b817a72727c9c0524
PrakhyatReddy <111814161+PrakhyatReddy@users.noreply.github.com>
2023-12-15 02:07:48 -0600

    Update view-standings-chart.php

--- view-standings-chart.php
+++ view-standings-chart.php
@@ -22,43 +22,57 @@
 
         const ctx = document.getElementById('myChart');
 
-new Chart(ctx, {
-    type: 'pie',
-    data: {
-        datasets: [{
-            data: [
-                <?php
-                while ($standing = $standings->fetch_assoc()) {
-                    echo $standing['wins'] . ", ";
+        new Chart(ctx, {
+            type: 'bar',  // Change the chart type to 'bar'
+            data: {
+                labels: [
+                    <?php
+                    $standings = selectStandings();    
+                    while ($standing = $standings->fetch_assoc()) {
+                        echo "'" . $standing['driver_name'] . "', ";
+                    }
+                    ?>
+                ],
+                datasets: [{
+                    label: 'Wins',
+                    data: [
+                        <?php
+                        $standings = selectStandings();    
+                        while ($standing = $standings->fetch_assoc()) {
+                            echo $standing['wins'] . ", ";
+                        }
+                        ?>
+                    ],
+                    backgroundColor: [
+                        '#F5EEEC',   // Color for the first bar
+                        '#B5A7A5',    // Color for the second bar
+                        '#FF390F',     // Color for the third bar
+                        // Add more colors if needed for additional bars
+                    ],
+                    borderColor: [
+                        '#F5EEEC',   // Border color for the first bar
+                        '#B5A7A5',    // Border color for the second bar
+                        '#FF390F',     // Border color for the third bar
+                        // Add more colors if needed for additional bars
+                    ],
+                    borderWidth: 1,
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                plugins: {
+                    legend: {
+                        labels: {
+                            color: '#000000' 
+                        }
+                    }
                 }
-                ?>
-            ],
-            backgroundColor: [
-                '#F5EEEC',   // Color for the first slice
-                '#B5A7A5',    // Color for the second slice
-                '#FF390F',     // Color for the third slice
-                // Add more colors if needed for additional slices
-            ],
-        }],
-        labels: [
-            <?php
-            $standings = selectStandings();    
-            while ($standing = $standings->fetch_assoc()) {
-                echo "'" . $standing['driver_name'] . "', ";
             }
-            ?>
-        ]
-    },
-    options: {
-        plugins: {
-            legend: {
-                labels: {
-                    color: '#000000' 
-                }
-            }
-        }
-    }
-});
+        });
     </script>
 </body>
 </html>
